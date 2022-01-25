904af9234e34c1f30ffb48f9eadd5b598669886f
Enosk <japaneseplay1994@gmail.com>
2022-01-25 08:14:47 +0000

    created Header

--- my-svelte-project/src/components/Header.svelte
+++ my-svelte-project/src/components/Header.svelte
@@ -1,6 +1,7 @@
 <header class="top-0 lef-0 w-full z-40 bg-gray-900 shadow fixed bordeer-b border-gray-200">
-    <div class="container ">
-        <span>
+    <div class="container mx-quto px-6 h-16 flex justify-between items-center">
+        <span class="font-semibold text-xl text-white tracking-tight">
+        TVTube for Svelte
         </span>
     </div>
 </header>
