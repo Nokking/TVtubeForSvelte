7e86b742b53d77580fe9360d0fc57111553479d4
Enosk <japaneseplay1994@gmail.com>
2022-01-27 04:24:34 +0000

    edit MainPage and formatted

--- my-svelte-project/src/pages/TVTubeSvelte.svelte
+++ my-svelte-project/src/pages/TVTubeSvelte.svelte
@@ -1,8 +1,14 @@
 <script lang="ts">
     import SearchBar from "../components/SearchBar.svelte";
+
+    let q = 'Javascript'
 </script>
 
 
 <form>
-    <SearchBar />
+    <SearchBar value={q} />
 </form>
+
+<div class="text-center mt-4">
+    { q }
+</div>
